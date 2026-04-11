be27c16ca11e900b2ebae0fb6be5022df07223a0
25P0412 <25P0412@eng.asu.edu.eg>
2026-04-11 22:19:46 +0200

    base: Created Database class containing ArrayLists
    Waiting for other classes to be ready.

---
+++ src/main/java/oop/project/hrs/backend/Database.java
@@ -0,0 +1,9 @@
+package oop.project.hrs.backend;
+
+import java.util.ArrayList;
+
+public class Database {
+    public static ArrayList<Guest> guests = new ArrayList<>();
+    public static ArrayList<String> usernames = new ArrayList<>(); //Use this one to check if a username is taken.
+    //rest of arrays;
+}
