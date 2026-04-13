ed77ca9ca29b60ad07ceab87134d95e272efb599
25P0412 <25P0412@eng.asu.edu.eg>
2026-04-13 14:02:19 +0200

    fix: Better encapsulation and added getter/setter  and search methods

--- src/main/java/oop/project/hrs/backend/Database.java
+++ src/main/java/oop/project/hrs/backend/Database.java
@@ -1,26 +1,40 @@
-package main.java.oop.project.hrs.backend;
+package oop.project.hrs.backend;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Database {
 
     private static ArrayList<Guest> guests = new ArrayList<>();
     private static ArrayList<String> usernames = new ArrayList<>(); //Use this one to check if a username is taken.
     //rest of arrays;
-
-    public static ArrayList<Guest> getGuests() {
-        return guests;
+    public static void addGuest(Guest guest) {
+        guests.add(guest);
     }
-
-    public static void setGuests(ArrayList<Guest> guests) {
-        Database.guests = guests;
+    public static void removeGuest(String username) {
+        guests.remove(getGuestByUsername(username));
+        usernames.remove(username);
     }
-
+    public static void addUsername(String username) {
+        usernames.add(username);
+    }
+    public static void removeUsername() {};
+    public static boolean usernameExists(String search) {
+        return usernames.contains(search);
+    }
     public static ArrayList<String> getUsernames() {
         return usernames;
     }
-
-    public static void setUsernames(ArrayList<String> usernames) {
-        Database.usernames = usernames;
+    public static List<Guest> getGuests() {
+        return Collections.unmodifiableList(guests);
+    }
+    public static Guest getGuestByUsername(String username) {
+        for (Guest g: guests) {
+            if (g.getUsername().equals(username)) {
+                return g;
+            }
+        }
+        return null;
     }
 }
