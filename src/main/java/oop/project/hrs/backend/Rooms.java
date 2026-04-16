42de56ac5ba6f70231c33de3a4ee5a332534f5b1
Marly Emad <marlyzakhary9@gmail.com>
2026-04-16 14:40:15 +0200

    Defined the data fields of the abstract class Rooms

---
+++ src/main/java/oop/project/hrs/backend/Rooms.java
@@ -0,0 +1,13 @@
+package oop.project.hrs.backend;
+
+public abstract class Rooms {
+    private int roomNum;
+    private int numOfGuests;
+    private int numOfBeds;
+    private double price;
+    private Guest guest;
+
+
+
+
+}
