af740c39146504a323d7fb575fdb309624781e36
Marly Emad <marlyzakhary9@gmail.com>
2026-04-16 15:11:21 +0200

    Implemented the Constructor & getters and setters methods of the abstract class Rooms

--- src/main/java/oop/project/hrs/backend/Rooms.java
+++ src/main/java/oop/project/hrs/backend/Rooms.java
@@ -7,7 +7,24 @@ public abstract class Rooms {
     private double price;
     private Guest guest;
 
+    //Constructor
+    public Rooms (int roomNum, int numOfGuests, int numOfBeds, double price, Guest guest){
+        this.roomNum=roomNum;
+        this.numOfGuests=numOfGuests;
+        this.numOfBeds=numOfBeds;
+        this.price=price;
+        this.guest=guest;
+    }
 
-
-
+    //Getters and setters methods
+    public int getRoomNum () {return roomNum;}
+    public void setRoomNum (int roomNum) {this.roomNum=roomNum;}
+    public int getNumOfGuests () {return numOfGuests;}
+    public void setNumOfGuests (int numOfGuests) {this.numOfGuests=numOfGuests;}
+    public int getNumOfBeds () {return numOfBeds;}
+    public void setNumOfBeds (int numOfBeds) {this.numOfBeds=numOfBeds;}
+    public double getPrice () {return price;}
+    public void setPrice (double price) {this.price=price;}
+    public Guest getGuest () {return guest;}
+    public void setGuest (Guest guest) {this.guest=guest;}
 }
