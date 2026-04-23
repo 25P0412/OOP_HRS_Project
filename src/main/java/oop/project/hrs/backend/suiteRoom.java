e26676c3ba1859aa4d18bf2faf8f8560c6455c24
Marly Emad <marlyzakhary9@gmail.com>
2026-04-23 21:24:27 +0200

    Added test data to the allRooms and hotelAmenities array lists.

--- src/main/java/oop/project/hrs/backend/suiteRoom.java
+++ src/main/java/oop/project/hrs/backend/suiteRoom.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class suiteRoom extends Rooms {
     //Constructor
     public suiteRoom (int roomNum, int numOfGuests, Guest guest, ArrayList <Amenity> masterSuiteAmenities){
-        //TODO: An error message to ensure that the number of guests does NOT exceed 5
+        //TODO: EXCEPTION HANDLING to ensure that the number of guests does NOT exceed 5
         super (RoomType.SUITE, Status.BOOKED, masterSuiteAmenities, roomNum, numOfGuests, 5, 7500.00, guest);
     }
 }
