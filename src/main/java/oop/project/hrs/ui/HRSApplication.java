229c390fd5249226429969a907f92d637d6e489a
salma <slmy3401@gmail.com>
2026-05-08 00:45:53 +0300

    Login interface

--- src/main/java/oop/project/hrs/ui/HRSApplication.java
+++ src/main/java/oop/project/hrs/ui/HRSApplication.java
@@ -4,22 +4,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import oop.project.hrs.backend.Database;
+
 import java.io.IOException;
 
 public class HRSApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GuestLogin.fxml")); //Change this one later to homepage.fxml
+        /* Loading the new custom login and register view */
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login & register.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hotel Reservation System - Login"); //Change this one too later
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hotel Reservation System");
         stage.setScene(scene);
-        stage.setResizable(false);
         stage.show();
     }
 
     public static void main(String[] args) {
+        Database.prePopulateData();
         launch();
     }
 }
