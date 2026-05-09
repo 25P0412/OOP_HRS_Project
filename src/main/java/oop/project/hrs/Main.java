1783a2c11fead84272beb73bf53643039a156f14
25P0412 <25P0412@eng.asu.edu.eg>
2026-05-09 07:17:51 +0300

    fix: fix logic in Application start

--- src/main/java/oop/project/hrs/Main.java
+++ src/main/java/oop/project/hrs/Main.java
@@ -5,6 +5,7 @@ import oop.project.hrs.ui.HRSApplication;
 
 public class Main {
     public static void main(String[] args) {
+        Database.prePopulateData();
         HRSApplication.main(args);
     }
 }
--- src/main/java/oop/project/hrs/ui/HRSApplication.java
+++ src/main/java/oop/project/hrs/ui/HRSApplication.java
@@ -2,55 +2,24 @@ package oop.project.hrs.ui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import oop.project.hrs.backend.Database;
-import oop.project.hrs.backend.Rooms;
-
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class HRSApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        /* Loading the new custom login and register view */
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ReservationDashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login & register.fxml")); //Change this one later to homepage.fxml
 
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hotel Reservation System");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        stage.setTitle("Hotel Reservation System - Login"); //Change this one too later
         stage.setScene(scene);
+        stage.setResizable(true);
         stage.show();
-
-        //ROOM BROWSING PART
-        try {
-            // 1. Load your FXML file
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/oop/project/hrs/ui/RoomBrowsing.fxml"));
-            Parent root = loader.load();
-
-            // 2. Access your Controller
-            RoomsController controller = loader.getController();
-
-            // 3. Prepare the data using the team's shared Database class
-            Database.prePopulateData(); // Fills the internal data
-
-            // 4. Pass the data to your controller
-            // Note: Ensure Database.getRooms() exists and returns an ArrayList<Rooms>
-            controller.setRoomsData(Database.getRooms());
-
-            // 5. Display the scene (Teammate will likely handle the Stage part)
-            stage.setScene(new Scene(root));
-            stage.show();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Error: Could not find the RoomBrowsing.fxml file.");
-        }
     }
 
     public static void main(String[] args) {
-        Database.prePopulateData();
         launch();
     }
 }
